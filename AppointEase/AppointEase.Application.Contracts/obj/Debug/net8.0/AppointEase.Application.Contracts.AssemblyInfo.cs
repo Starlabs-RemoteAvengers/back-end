47e64dae234c4ce4e3c32720481845b845f7a6d4
ValdrinH <95773922+ValdrinH@users.noreply.github.com>
2024-02-12 13:42:42 +0100

    feat: Fixed Architecture

--- AppointEase/AppointEase.Application.Contracts/obj/Debug/net8.0/AppointEase.Application.Contracts.AssemblyInfo.cs
+++ AppointEase/AppointEase.Application.Contracts/obj/Debug/net8.0/AppointEase.Application.Contracts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppointEase.Application.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97adb87ff8811f0a319d9eeadedb7a7bfa207624")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppointEase.Application.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppointEase.Application.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AppointEase/AppointEase.Data/DataInjectionServices.cs
@@ -0,0 +1,30 @@
+﻿using AppointEase.Application.Contracts.Interfaces;
+using AppointEase.Data.Data;
+using AppointEase.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointEase.Data
+{
+    public static class DataInjectionServices
+    {
+        public static void AddDataServices(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            serviceDescriptors.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")
+                );
+            });
+
+            serviceDescriptors.AddScoped<DataContext>();
+            serviceDescriptors.AddScoped<IPersonRepository, PersonRepository>();
+        }
+    }
+}
