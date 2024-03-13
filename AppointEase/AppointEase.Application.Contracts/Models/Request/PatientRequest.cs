9604522a08e7e5addd5255a58936f788e477a43f
ValdrinH <95773922+ValdrinH@users.noreply.github.com>
2024-03-13 15:09:59 +0100

    feat: Added Email Content

--- AppointEase/AppointEase.Application.Contracts/Models/Request/PatientRequest.cs
+++ AppointEase/AppointEase.Application.Contracts/Models/Request/PatientRequest.cs
@@ -22,5 +22,8 @@ namespace AppointEase.Application.Contracts.Models.Request
         public string PhoneNumber { get; set; }
         public string Gender { get; set; }
         public DateOnly DateOfBirth { get; set; }
+
+        public string? PhotoData { get; set; }
+        public string? PhotoFormat { get; set; }
     }
 }
