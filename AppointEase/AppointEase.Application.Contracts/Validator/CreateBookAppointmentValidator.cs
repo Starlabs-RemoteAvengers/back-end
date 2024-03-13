0c9ef5f65c03115895b86203af96d0e9c0b7fb8b
Dita Simnica <ditasimnica@hotmail.com>
2024-03-13 10:41:30 +0100

    feat: implemented bookappointment

---
+++ AppointEase/AppointEase.Application.Contracts/Validator/CreateBookAppointmentValidator.cs
@@ -0,0 +1,21 @@
+﻿using AppointEase.Application.Contracts.Models.Request;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointEase.Application.Contracts.Validator
+{
+    public class CreateBookAppointmentValidator : AbstractValidator<BookAppointmentRequest>
+    {
+        public CreateBookAppointmentValidator()
+        {
+            RuleFor(x => x.AppointmentSlotId).NotEmpty().WithMessage("AppointmentSlotId is required.");
+            RuleFor(x => x.PatientId).NotEmpty().WithMessage("PatientId is required.");
+            RuleFor(x => x.MeetingReason).NotEmpty().WithMessage("MeetingReason is required.");
+            RuleFor(x => x.MeetingRequestDescription).NotEmpty().WithMessage("MeetingRequestDescription is required.");
+        }
+    }
+}
